1fd6a6c4c53e99c40aa9c2eb27b8fb71f19107f6
Yasutaka Ohmura <nishikimachi.hokkaido@gmail.com>
2019-12-07 12:21:17 +0900

    シングルAZからマルチに修正

--- main.tf
+++ main.tf
@@ -28,6 +28,16 @@ resource "aws_subnet" "public-subnet-1a" {
   }
 }
 
+resource "aws_subnet" "public-subnet-1c" {
+  vpc_id                  = aws_vpc.awssaa-vpc.id
+  cidr_block              = "172.16.11.0/24"
+  map_public_ip_on_launch = true # インスタンスにパブリックIP自動割り当て有効
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "public-subnet-1c"
+  }
+}
+
 # サブネット作成 Private
 resource "aws_subnet" "private-subnet-1a" {
   vpc_id                  = aws_vpc.awssaa-vpc.id
@@ -39,6 +49,15 @@ resource "aws_subnet" "private-subnet-1a" {
   }
 }
 
+resource "aws_subnet" "private-subnet-1c" {
+  vpc_id                  = aws_vpc.awssaa-vpc.id
+  cidr_block              = "172.16.21.0/24"
+  map_public_ip_on_launch = false # インスタンスにパブリックIP自動割り当てない
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "private-subnet-1c"
+  }
+}
 # インターネットゲートウェイ作成
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.awssaa-vpc.id
@@ -68,6 +87,11 @@ resource "aws_route_table_association" "public-1a" {
   route_table_id = aws_route_table.public-rt.id
 }
 
+resource "aws_route_table_association" "public-1c" {
+  subnet_id      = aws_subnet.public-subnet-1c.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
 # NATゲートウェイ作成 Privateサブネット用
 # 先にNATゲートウェイ用のEIPを作成する必要がある
 resource "aws_eip" "nat-gateway-eip1" {
@@ -75,6 +99,11 @@ resource "aws_eip" "nat-gateway-eip1" {
   depends_on = [aws_internet_gateway.igw]
   # NATゲートウェイは暗黙的にIGWに依存している。
   # depends onでIGW作成後にNATゲートウェイ作成することを保証する
+}
+
+resource "aws_eip" "nat-gateway-eip2" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
 }
 
 # NATゲートウェイ作成
@@ -86,7 +115,14 @@ resource "aws_nat_gateway" "nat-gateway-1a" {
     Name = "awssaa-nat-gateway-1a"
   }
 }
-
+resource "aws_nat_gateway" "nat-gateway-1c" {
+  allocation_id = aws_eip.nat-gateway-eip2.id
+  subnet_id     = aws_subnet.public-subnet-1c.id # NATゲートウェイ自体はPublicに置かれる
+  depends_on    = [aws_internet_gateway.igw]
+  tags = {
+    Name = "awssaa-nat-gateway-1c"
+  }
+}
 # ルートテーブル作成 Private
 resource "aws_route_table" "private-rt1" {
   vpc_id = aws_vpc.awssaa-vpc.id
@@ -95,6 +131,12 @@ resource "aws_route_table" "private-rt1" {
   }
 }
 
+resource "aws_route_table" "private-rt2" {
+  vpc_id = aws_vpc.awssaa-vpc.id
+  tags = {
+    Name = "awssaa-private-rt2"
+  }
+}
 # ルーティング設定 Private
 resource "aws_route" "private-1a" {
   route_table_id         = aws_route_table.private-rt1.id
@@ -102,7 +144,18 @@ resource "aws_route" "private-1a" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route" "private-1c" {
+  route_table_id         = aws_route_table.private-rt2.id
+  nat_gateway_id         = aws_nat_gateway.nat-gateway-1c.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.private-subnet-1a.id
   route_table_id = aws_route_table.private-rt1.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private-subnet-1c.id
+  route_table_id = aws_route_table.private-rt2.id
 }
